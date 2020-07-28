36ddda464f04c10bb3461cdadd72e071f8e1eaf7
Muchlis Adhi <adi.wiratama@gmail.com>
2020-07-28 14:11:19 +0700

    Add Skill To Character with Many to Many relation

--- Dtos/Character/GetCharacterDto.cs
+++ Dtos/Character/GetCharacterDto.cs
@@ -1,5 +1,7 @@
 using dotnet_rpg.Models;
 using dotnet_rpg.Dtos.Weapon;
+using System.Collections.Generic;
+using dotnet_rpg.Dtos.Skill;
 
 namespace dotnet_rpg.Dtos.Character
 {
@@ -13,5 +15,6 @@ namespace dotnet_rpg.Dtos.Character
         public int Intelligence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
         public GetWeaponDto Weapon { get; set; }
+        public List<GetSkillDto> Skills { get; set; }
     }
 }
